f8e2ed57293c91e57852cdf23492dd550eb159f8
Zohn <liangzhou.liu@honeywell.com>
2018-07-17 18:28:05 +0800

    update DeviceCatalogImpl.cs

--- GrpcAgent/WebsocketRpcServer/DeviceCatalogImpl.cs
+++ GrpcAgent/WebsocketRpcServer/DeviceCatalogImpl.cs
@@ -5,17 +5,30 @@ using System.Threading.Tasks;
 using Grpc.Core;
 using GrpcDeviceCatalog;
 using SIPSorcery.GB28181.Servers;
-//using SIPSorcery.GB28181.Servers.SIPMessage;
+using SIPSorcery.GB28181.Servers.SIPMessage;
+using SIPSorcery.GB28181.Sys.XML;
 
 namespace GrpcAgent.WebsocketRpcServer
 {
     public class DeviceCatalogImpl : DeviceCatalog.DeviceCatalogBase
     {
         private ISIPCatalogCore _sipCatalogCore = null;
+        private ISipMessageCore _sipMessageCore = null;
+        private Dictionary<string, Catalog> _catalogs = new Dictionary<string, Catalog>();
 
-        public DeviceCatalogImpl(ISIPCatalogCore sipCatalogCore)
+        public DeviceCatalogImpl(ISIPCatalogCore sipCatalogCore, ISipMessageCore sipMessageCore)
         {
             _sipCatalogCore = sipCatalogCore;
+            _sipMessageCore = sipMessageCore;
+            _sipMessageCore.OnCatalogReceived += _sipMessageCore_OnCatalogReceived;
+        }
+
+        private void _sipMessageCore_OnCatalogReceived(Catalog cata)
+        {
+            if (!_catalogs.ContainsKey(cata.DeviceID))
+            {
+                _catalogs.Add(cata.DeviceID, cata);
+            }
         }
 
         // Server side handler of the SayHello RPC
@@ -23,6 +36,25 @@ namespace GrpcAgent.WebsocketRpcServer
         {
             _sipCatalogCore.GetCatalog(request.Deviceid);
             string x = "Status: 200 OK";
+            Catalog _Catalog = null;
+            while (true)
+            {
+                foreach (Catalog obj in _catalogs.Values)
+                {
+                    if (request.Deviceid.Equals(obj.DeviceID))
+                    {
+                        _Catalog = obj;
+                    }
+                }
+                if (_Catalog == null)
+                {
+                    System.Threading.Thread.Sleep(500);
+                }
+                else
+                {
+                    break;
+                }
+            }
             return Task.FromResult(new GetCatalogReply { Message = x });
         }
 
